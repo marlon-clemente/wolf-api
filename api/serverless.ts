a68b59574bc4fb95fbbbb2bc39dad45a2f617bc5
marlon-clemente <marlon.klemente@gmail.com>
2023-08-12 20:45:33 -0300

    update

---
+++ api/serverless.ts
@@ -0,0 +1,40 @@
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import fastify from "fastify";
+import bcrypt from "fastify-bcrypt";
+
+import { authRoutes } from " ../src/app/routes/authRoutes";
+import { configDotenv } from "dotenv";
+import path from "path";
+import { companies } from "../src/app/routes/company";
+import { statusRoutes } from "../src/app/routes/statusRoutes";
+
+const app = fastify({ logger: true });
+const envFilePath =
+  process.env.NODE_ENV === "production" ? ".env.prod" : ".env";
+
+configDotenv({
+  path: path.resolve(__dirname, envFilePath),
+});
+
+app.register(cors, {
+  // origin: ['http://localhost:3333'],
+  origin: [],
+});
+
+app.register(bcrypt, {
+  saltWorkFactor: 10,
+});
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET_KEY || "123s4567885699",
+});
+
+app.register(authRoutes);
+app.register(statusRoutes);
+app.register(companies);
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
