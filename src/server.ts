c6423df114fdd661cc164c94e6b58da3cac7e31f
marlon-clemente <marlon.klemente@gmail.com>
2023-08-12 14:00:48 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,49 @@
-import bcrypt from 'fastify-bcrypt'
-import fastify from 'fastify'
-import jwt from '@fastify/jwt'
-import cors from '@fastify/cors'
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import fastify from "fastify";
+import bcrypt from "fastify-bcrypt";
 
-import { authRoutes } from './routes/authRoutes'
-import { companies } from './routes/company'
-import { statusRoutes } from './routes/statusRoutes'
-import { configDotenv } from 'dotenv'
-import path from 'path'
+import { configDotenv } from "dotenv";
+import path from "path";
+import { authRoutes } from "./routes/authRoutes";
+import { companies } from "./routes/company";
+import { statusRoutes } from "./routes/statusRoutes";
 
-const app = fastify()
-const envFilePath = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env'
+export const init = () => {
+  const app = fastify();
+  const envFilePath =
+    process.env.NODE_ENV === "production" ? ".env.prod" : ".env";
 
-configDotenv({
-  path: path.resolve(__dirname, envFilePath),
-})
+  configDotenv({
+    path: path.resolve(__dirname, envFilePath),
+  });
 
-app.register(cors, {
-  // origin: ['http://localhost:3333'],
-  origin: [],
-})
+  app.register(cors, {
+    // origin: ['http://localhost:3333'],
+    origin: [],
+  });
 
-app.register(bcrypt, {
-  saltWorkFactor: 10,
-})
+  app.register(bcrypt, {
+    saltWorkFactor: 10,
+  });
 
-app.register(jwt, {
-  secret: process.env.JWT_SECRET_KEY || '123s4567885699',
-})
+  app.register(jwt, {
+    secret: process.env.JWT_SECRET_KEY || "123s4567885699",
+  });
 
-console.log('degund => process.env.JWT_SECRET_KEY', process.env.JWT_SECRET_KEY)
+  app.register(authRoutes);
+  app.register(statusRoutes);
+  app.register(companies);
 
-app.register(authRoutes)
-app.register(statusRoutes)
-app.register(companies)
+  return app;
+};
 
-app
-  .listen({
-    port:
-      process.env.NODE_ENV === 'production' ? Number(process.env.PORT) : 3333,
-  })
-  .then(() => {
-    console.log('running...')
-  })
+if (require.main === module) {
+  init()
+    .listen({
+      port: 3333,
+    })
+    .then(() => {
+      console.log("running...");
+    });
+}
