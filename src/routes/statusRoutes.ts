ec9fa81c207324294d1c83872f962d45c0d00298
marlon-clemente <marlon.klemente@gmail.com>
2023-10-25 21:49:13 -0300

    refactor prisma

--- src/routes/statusRoutes.ts
+++ src/routes/statusRoutes.ts
@@ -1,20 +1,7 @@
 import { FastifyInstance } from 'fastify'
-import { prisma } from '../lib/prisma'
 
 export async function statusRoutes(server: FastifyInstance) {
   server.get('/ping', async (_, resp) => {
     resp.code(200).send({ message: 'pong' })
-  })
-
-  server.get('/database', async (_, resp) => {
-    const data = await prisma.company.count()
-
-    if (data >= 0) {
-      return resp.code(200).send({ message: 'pong' })
-    }
-
-    resp.code(500).send({
-      message: 'Database not connected',
-    })
   })
 }
